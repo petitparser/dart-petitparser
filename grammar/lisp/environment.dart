1ff078d17714006b8635b01fae9222b5ef0d5a7c
Lukas Renggli <renggli@gmail.com>
2012-07-15 17:10:28 +0200

    - added implementation of environment

--- grammar/lisp/environment.dart
+++ grammar/lisp/environment.dart
@@ -1,7 +1,48 @@
 // Copyright (c) 2012, Lukas Renggli <renggli@gmail.com>
 
-class Environment {
+abstract class Environment {
 
-  Environment();
+  /** The internal environment bindings. */
+  final Map<SymbolCell, Cell> _bindings;
+
+  /** Constructor for the environment. */
+  Environment() : _bindings = new Map();
+
+  /** Constructor for a nested environment. */
+  Environment create() => new NestedEnvironment(this);
+
+  /** Returns the cell defined by a [key]. */
+  Cell operator [](SymbolCell key) {
+    var value = _bindings[key];
+    return value != null ? value : _notFound(key);
+  }
+
+  /** Defines or redefines the cell with [value] of a [key]. */
+  void operator []=(SymbolCell key, Cell value) {
+    _bindings[key] = value;
+  }
+
+  /** Abstract behavior called when an non-existing binding is accessed. */
+  abstract Cell _notFound(SymbolCell key);
+
+}
+
+class RootEnvironment extends Environment {
+
+  /** Return [NULL] if the value does not exist. */
+  _notFound(SymbolCell key) => NULL;
+
+}
+
+class NestedEnvironment extends Environment {
+
+  /** The owning environemnt. */
+  final Environment _owner;
+
+  /** Constructs a nested environment. */
+  NestedEnvironment(this._owner);
+
+  /** Lookup values in the parent environment. */
+  _notFound(SymbolCell key) => _owner[key];
 
 }
