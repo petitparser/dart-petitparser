d64b0a443e104448f2f061bfbf2dcb748fc4429a
Lukas Renggli <renggli@gmail.com>
2012-07-21 21:33:32 +0200

    - fixed printing of cons cells that are not valid

--- grammar/lisp/cells.dart
+++ grammar/lisp/cells.dart
@@ -52,17 +52,26 @@ class Cons {
   Dynamic get tail()             => _tail;
           set tail(Dynamic tail) => _tail = tail;
 
+  /** Compare the cells. */
+  bool operator ==(Cons cons) {
+    return cons is Cons && head == cons.head && tail == cons.tail;
+  }
+
   /** Returns the string representation of the cons. */
   String toString() {
     StringBuffer buffer = new StringBuffer();
     buffer.add('(');
     var current = this;
-    while (current != null) {
+    while (current is Cons) {
       buffer.add(current.head.toString());
-      if (current.tail != null) {
+      current = current.tail;
+      if (current != null) {
         buffer.add(' ');
       }
-      current = current.tail;
+    }
+    if (current != null) {
+      buffer.add('. ');
+      buffer.add(current);
     }
     buffer.add(')');
     return buffer.toString();
