385eb0046aef17265eb476c4f5a28d68cf3a7cd2
Lukas Renggli <renggli@gmail.com>
2012-08-25 19:07:40 +0200

    - add lisp tests to all tests

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -7,9 +7,11 @@
 #import('petitparser_tests.dart', prefix: 'petitparser');
 #import('json_tests.dart', prefix: 'json');
 #import('xml_tests.dart', prefix: 'xml');
+#import('lisp_tests.dart', prefix: 'lisp');
 
 void main() {
   group('PetitParser', petitparser.main);
   group('JSON', json.main);
   group('XML', xml.main);
+  group('LISP', lisp.main);
 }
