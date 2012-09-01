c5195e9fa7b6256fe8404e117a544d5b7ccb4f00
Lukas Renggli <renggli@gmail.com>
2012-09-01 12:56:20 +0200

    - improved the jenkins runner

--- test/all_tests_jenkins.dart
+++ test/all_tests_jenkins.dart
@@ -12,28 +12,38 @@ void main() {
 }
 
 class JenkinsConfiguration extends Configuration {
-  void onDone(int passed, int failed, int errors, List<TestCase> results, String uncaughtError) {
-    print('<?xml version="1.0" encoding="UTF-8" ?>');
-    print('<testsuite name="All tests" tests="${results.length}" failures="$failed" errors="$errors">');
-    for (var testcase in results) {
-      print('  <testcase name="${_xml(testcase.description)}">');
-      if (testcase.result == 'fail') {
-        print('    <failure>${_xml(testcase.message)}</failure>');
-      } else if (testcase.result == 'error') {
-        print('    <error>${_xml(testcase.message)}</error>');
-      }
-      if (testcase.stackTrace != null && testcase.stackTrace != '') {
-        print('    <system-err>${_xml(testcase.stackTrace)}</system-err>');
-      }
-      print('  </testcase>');
-    }
-    print('</testsuite>');
-  }
-}
 
-String _xml(String string) {
-  return string.replaceAll('&', '&amp;')
-               .replaceAll('<', '&lt;')
-               .replaceAll('>', '&gt;')
-               .replaceAll('"', '&quot;');
+  bool printed = false;
+
+  void onDone(int passed, int failed, int errors, List<TestCase> results, String uncaughtError) {
+    if (!printed) {
+      print('<?xml version="1.0" encoding="UTF-8" ?>');
+      print('<testsuite name="All tests" tests="${results.length}" failures="$failed" errors="$errors">');
+      for (var testcase in results) {
+        print('  <testcase name="${_xml(testcase.description)}">');
+        if (testcase.result == 'fail') {
+          print('    <failure>${_xml(testcase.message)}</failure>');
+        } else if (testcase.result == 'error') {
+          print('    <error>${_xml(testcase.message)}</error>');
+        }
+        if (testcase.stackTrace != null && testcase.stackTrace != '') {
+          print('    <system-err>${_xml(testcase.stackTrace)}</system-err>');
+        }
+        print('  </testcase>');
+      }
+      if (uncaughtError != null && uncaughtError != '') {
+        print('  <system-err>${_xml(uncaughtError)}</system-err>');
+      }
+      print('</testsuite>');
+      printed = true;
+    }
+  }
+
+  String _xml(String string) {
+    return string.replaceAll('&', '&amp;')
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
+        .replaceAll('"', '&quot;');
+  }
+
 }
