89f82c3998c8df177dde93d65feda6c806b79f82
Kevin Moore <kevin@thinkpixellab.com>
2013-03-30 19:01:47 -0400

    Add hop_runner

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -3,12 +3,21 @@
 library all_tests;
 
 import 'package:unittest/unittest.dart';
+import 'package:unittest/vm_config.dart';
 import 'json_tests.dart' as json;
 import 'lisp_tests.dart' as lisp;
 import 'petitparser_tests.dart' as petitparser;
 import 'xml_tests.dart' as xml;
 
-void main() {
+main() {
+  final config = new VMConfiguration();
+  testCore(config);
+}
+
+void testCore(Configuration config) {
+  configure(config);
+  groupSep = ' - ';
+
   group('PetitParser', petitparser.main);
   group('JSON', json.main);
   group('XML', xml.main);
