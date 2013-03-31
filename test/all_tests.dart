db0200edfe8994231ccbc70f76791f5fb9c3df1b
Lukas Renggli <renggli@gmail.com>
2013-03-31 13:46:21 +0200

    - renamed tests to match rest of package naming

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -5,11 +5,11 @@ library all_tests;
 import 'package:unittest/unittest.dart';
 import 'json_tests.dart' as json;
 import 'lisp_tests.dart' as lisp;
-import 'petitparser_tests.dart' as petitparser;
+import 'core_tests.dart' as core;
 import 'xml_tests.dart' as xml;
 
 void main() {
-  group('PetitParser', petitparser.main);
+  group('PetitParser', core.main);
   group('JSON', json.main);
   group('XML', xml.main);
   group('LISP', lisp.main);
