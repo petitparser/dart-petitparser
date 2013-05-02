0412e7abd8cfa105d7d438af5023bf5c279a01a5
Kevin Moore <kevin@thinkpixellab.com>
2013-05-02 09:04:33 -0400

    share the instance of the grammar for all tests

--- test/dart_test.dart
+++ test/dart_test.dart
@@ -10,42 +10,42 @@ import 'package:unittest/unittest.dart';
 
 void generateTests(String title, String path) {
   group(title, () {
-    var dart = new DartGrammar();
     new Directory(path)
       .listSync(recursive: true, followLinks: false)
       .where((file) => file is File && file.path.endsWith('.dart'))
       .forEach((file) {
         test(file.path, () {
-          var result = dart.parse(file.readAsStringSync());
+          var result = _dart.parse(file.readAsStringSync());
           if (result.isFailure) fail(result.toString());
         });
       });
   });
 }
 
+final _dart = new DartGrammar();
+
 void main() {
-  var dart = new DartGrammar();
   test('basic files', () {
-    expect(dart.accept('library test;'), isTrue);
-    expect(dart.accept('library test; void main() { }'), isTrue);
-    expect(dart.accept('library test; void main() { print(2 + 3); }'), isTrue);
+    expect(_dart.accept('library test;'), isTrue);
+    expect(_dart.accept('library test; void main() { }'), isTrue);
+    expect(_dart.accept('library test; void main() { print(2 + 3); }'), isTrue);
   });
   test('basic whitespace', () {
-    expect(dart.accept('library test;'), isTrue);
-    expect(dart.accept('  library test;'), isTrue);
-    expect(dart.accept('library test;  '), isTrue);
-    expect(dart.accept('library  test ;'), isTrue);
+    expect(_dart.accept('library test;'), isTrue);
+    expect(_dart.accept('  library test;'), isTrue);
+    expect(_dart.accept('library test;  '), isTrue);
+    expect(_dart.accept('library  test ;'), isTrue);
   });
   test('single line comment', () {
-    expect(dart.accept('library test;'), isTrue);
-    expect(dart.accept('library// foo\ntest;'), isTrue);
-    expect(dart.accept('library test // foo \n;'), isTrue);
-    expect(dart.accept('library test; // foo'), isTrue);
+    expect(_dart.accept('library test;'), isTrue);
+    expect(_dart.accept('library// foo\ntest;'), isTrue);
+    expect(_dart.accept('library test // foo \n;'), isTrue);
+    expect(_dart.accept('library test; // foo'), isTrue);
   });
   test('multi line comment', () {
-    expect(dart.accept('/* foo */ library test;'), isTrue);
-    expect(dart.accept('library /* foo */ test;'), isTrue);
-    expect(dart.accept('library test; /* foo */'), isTrue);
+    expect(_dart.accept('/* foo */ library test;'), isTrue);
+    expect(_dart.accept('library /* foo */ test;'), isTrue);
+    expect(_dart.accept('library test; /* foo */'), isTrue);
   });
   // generateTests('Dart SDK Sources', '/Applications/Dart/dart-sdk');
   generateTests('PetitParser Sources', '.');
