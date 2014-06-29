2238ee307a0010f976179315c0ceead55168ec4c
Lukas Renggli <renggli@gmail.com>
2014-06-29 14:59:42 +0200

    - finally removed the long deprecated xml package, that is now maintained separately

--- test/all_test.dart
+++ test/all_test.dart
@@ -8,7 +8,6 @@ import 'json_test.dart' as json_test;
 import 'lisp_test.dart' as lisp_test;
 import 'smalltalk_test.dart' as smalltalk_test;
 import 'test_test.dart' as test_test;
-import 'xml_test.dart' as xml_test;
 
 void main() {
   group('PetitParser', core_test.main);
@@ -18,5 +17,4 @@ void main() {
   group('Lisp', lisp_test.main);
   group('Smalltalk', smalltalk_test.main);
   group('Test', test_test.main);
-  group('XML', xml_test.main);
 }
