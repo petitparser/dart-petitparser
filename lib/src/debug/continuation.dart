02bf74f0c19aa3e08e58e5ebcfac23bf1607b430
Lukas Renggli <renggli@gmail.com>
2014-06-29 13:22:21 +0200

    - improved the comment of the continuation parser

--- lib/src/debug/continuation.dart
+++ lib/src/debug/continuation.dart
@@ -1,13 +1,29 @@
 part of debug;
 
 /**
- * Handler function of a [ContinuationParser].
+ * Handler function for the [ContinuationParser].
  */
-typedef Result ContinuationHandler(Context context, Function continuation);
+typedef Result ContinuationHandler(Context context, Result continuation(Context context));
 
 /**
- * Continuation parser that when executed allows the handler to
- * dynamically wrap its delegate.
+ * Continuation parser that when activated captures a continuation function
+ * and passes it together with the current context into the handler.
+ *
+ * Handlers are not required to call the continuation, but can completely ignore
+ * it, call it multiple times, and/or store it away for later use. Similarly
+ * handlers can modify the current context and/or modify the returned result.
+ *
+ * The following example shows a simple wrapper. Messages are printed before and
+ * after the `digit()` parser is activated:
+ *
+ *    var wrapped = digit();
+ *    var parser = new ContinuationParser(wrapped, (context, continuation) {
+ *      print('Parser will be activated, the context is $context.');
+ *      var result = continuation(context);
+ *      print('Parser was activated, the result is $result.');
+ *      return result;
+ *    });
+ *
  */
 class ContinuationParser extends DelegateParser {
 
