a8718c0d086f72ba668d85b2eeaf87a0255713ed
Lukas Renggli <renggli@gmail.com>
2014-08-03 11:59:42 +0200

    - optimize the optimization code

--- lib/src/reflection/optimize.dart
+++ lib/src/reflection/optimize.dart
@@ -1,31 +1,31 @@
- part of reflection;
+part of reflection;
 
- /**
-  * Returns a copy of [parser] with all setable parsers removed.
-  */
- Parser removeSetables(Parser parser) {
-   return transformParser(parser, (each) {
-     while (each is SetableParser) {
-       each = each.children.first;
-     }
-     return each;
-   });
- }
+/**
+ * Returns a copy of [parser] with all setable parsers removed.
+ */
+Parser removeSetables(Parser parser) {
+  return transformParser(parser, (each) {
+    while (each is SetableParser) {
+      each = each.children.first;
+    }
+    return each;
+  });
+}
 
- /**
-  * Returns a copy of [parser] with all duplicates parsers collapsed.
-  */
- Parser removeDuplicates(Parser parser) {
-   var uniques = new Set();
-   return transformParser(parser, (source) {
-     var target = uniques.firstWhere((each) {
-       return source != each && source.equals(each);
-     }, orElse: () => null);
-     if (target == null) {
-       uniques.add(source);
-       return source;
-     } else {
-       return target;
-     }
-   });
- }
+/**
+ * Returns a copy of [parser] with all duplicates parsers collapsed.
+ */
+Parser removeDuplicates(Parser parser) {
+  var uniques = new Set();
+  return transformParser(parser, (source) {
+    var target = uniques.firstWhere((each) {
+      return source != each && source.equals(each);
+    }, orElse: () => null);
+    if (target == null) {
+      uniques.add(source);
+      return source;
+    } else {
+      return target;
+    }
+  });
+}
