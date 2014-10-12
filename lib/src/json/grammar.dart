2c8e6761bf0476ef44ded3660f117f33901cc5a2
Lukas Renggli <renggli@gmail.com>
2014-10-12 16:03:21 +0200

    Adapt the JSON grammar to use the new grammar definition infrastructre.

--- lib/src/json/grammar.dart
+++ lib/src/json/grammar.dart
@@ -1,9 +1,16 @@
 part of json;
 
 /**
+ * JSON grammar.
+ */
+class JsonGrammar extends GrammarParser {
+  JsonGrammar() : super(new JsonGrammarDefinition());
+}
+
+/**
  * JSON grammar definition.
  */
-class JsonGrammar extends CompositeParser {
+class JsonGrammarDefinition extends GrammarDefinition {
 
   final _escapeTable = const {
     '\\': '\\',
@@ -16,60 +23,47 @@ class JsonGrammar extends CompositeParser {
     't': '\t'
   };
 
-  @override
-  void initialize() {
-    def('start', ref('value').end());
+  start() => ref(value).end();
+  token(p) => p.flatten().trim();
 
-    def('array',
-      char('[').trim()
-        .seq(ref('elements').optional())
-        .seq(char(']').trim()));
-    def('elements',
-      ref('value').separatedBy(char(',').trim(), includeSeparators: false));
-    def('members',
-      ref('pair').separatedBy(char(',').trim(), includeSeparators: false));
-    def('object',
-      char('{').trim()
-        .seq(ref('members').optional())
-        .seq(char('}').trim()));
-    def('pair',
-      ref('stringToken')
-        .seq(char(':').trim())
-        .seq(ref('value')));
-    def('value',
-      ref('stringToken')
-        .or(ref('numberToken'))
-        .or(ref('object'))
-        .or(ref('array'))
-        .or(ref('trueToken'))
-        .or(ref('falseToken'))
-        .or(ref('nullToken')));
+  array() => ref(token, char('['))
+      & ref(elements).optional()
+      & ref(token, char(']'));
+  elements() => ref(value).separatedBy(ref(token, char(',')), includeSeparators: false);
+  members() => ref(pair).separatedBy(ref(token, char(',')), includeSeparators: false);
+  object() => ref(token, char('{'))
+      & ref(members).optional()
+      & ref(token, char('}'));
+  pair() => ref(stringToken)
+      & ref(token, char(':'))
+      & ref(value);
+  value() => ref(stringToken)
+      | ref(numberToken)
+      | ref(object)
+      | ref(array)
+      | ref(trueToken)
+      | ref(falseToken)
+      | ref(nullToken);
 
-    def('trueToken', string('true').flatten().trim());
-    def('falseToken', string('false').flatten().trim());
-    def('nullToken', string('null').flatten().trim());
-    def('stringToken', ref('stringPrimitive').flatten().trim());
-    def('numberToken', ref('numberPrimitive').flatten().trim());
+  trueToken() => ref(token, string('true'));
+  falseToken() => ref(token, string('false'));
+  nullToken() => ref(token, string('null'));
+  stringToken() => ref(token, ref(stringPrimitive));
+  numberToken() => ref(token, ref(numberPrimitive));
 
-    def('characterPrimitive',
-      ref('characterEscape')
-        .or(ref('characterOctal'))
-        .or(ref('characterNormal')));
-    def('characterEscape',
-      char('\\').seq(anyIn(new List.from(_escapeTable.keys))));
-    def('characterNormal',
-      anyIn('"\\').neg());
-    def('characterOctal',
-      string('\\u').seq(pattern("0-9A-Fa-f").times(4).flatten()));
-    def('numberPrimitive',
-      char('-').optional()
-        .seq(char('0').or(digit().plus()))
-        .seq(char('.').seq(digit().plus()).optional())
-        .seq(anyIn('eE').seq(anyIn('-+').optional()).seq(digit().plus()).optional()));
-    def('stringPrimitive',
-      char('"')
-        .seq(ref('characterPrimitive').star())
-        .seq(char('"')));
-  }
+  characterPrimitive() => ref(characterEscape)
+      | ref(characterOctal)
+      | ref(characterNormal);
+  characterEscape() => char('\\')
+      & anyIn(new List.from(_escapeTable.keys));
+  characterNormal() => anyIn('"\\').neg();
+  characterOctal() => string('\\u').seq(pattern("0-9A-Fa-f").times(4).flatten());
+  numberPrimitive() => char('-').optional()
+      & char('0').or(digit().plus())
+      & char('.').seq(digit().plus()).optional()
+      & anyIn('eE').seq(anyIn('-+').optional()).seq(digit().plus()).optional();
+  stringPrimitive() => char('"')
+      & ref(characterPrimitive).star()
+      & char('"');
 
 }
--- lib/src/json/parser.dart
+++ lib/src/json/parser.dart
@@ -1,45 +1,46 @@
 part of json;
 
 /**
+ * JSON parser.
+ */
+class JsonParser extends GrammarParser {
+  JsonParser() : super(new JsonParserDefinition());
+}
+
+/**
  * JSON parser definition.
  */
-class JsonParser extends JsonGrammar {
+class JsonParserDefinition extends JsonGrammarDefinition {
 
-  @override
-  void initialize() {
-    super.initialize();
-
-    action('array', (each) => each[1] != null ? each[1] : new List());
-    action('object', (each) {
-      var result = new LinkedHashMap();
-      if (each[1] != null) {
-        for (var element in each[1]) {
-          result[element[0]] = element[2];
-        }
+  array() => super.array().map((each) => each[1] != null ? each[1] : new List());
+  object() => super.object().map((each) {
+    var result = new LinkedHashMap();
+    if (each[1] != null) {
+      for (var element in each[1]) {
+        result[element[0]] = element[2];
       }
-      return result;
-    });
+    }
+    return result;
+  });
 
-    action('trueToken', (each) => true);
-    action('falseToken', (each) => false);
-    action('nullToken', (each) => null);
-    redef('stringToken', (parser) => ref('stringPrimitive').trim());
-    action('numberToken', (each) {
-      var floating = double.parse(each);
-      var integral = floating.toInt();
-      if (floating == integral && each.indexOf('.') == -1) {
-        return integral;
-      } else {
-        return floating;
-      }
-    });
+  trueToken() => super.trueToken().map((each) => true);
+  falseToken() => super.falseToken().map((each) => false);
+  nullToken() => super.nullToken().map((each) => null);
+  stringToken() => ref(stringPrimitive).trim();
+  numberToken() => super.numberToken().map((each) {
+    var floating = double.parse(each);
+    var integral = floating.toInt();
+    if (floating == integral && each.indexOf('.') == -1) {
+      return integral;
+    } else {
+      return floating;
+    }
+  });
 
-    action('stringPrimitive', (each) => each[1].join(''));
-    action('characterEscape', (each) => _escapeTable[each[1]]);
-    action('characterOctal', (each) {
-      throw new UnsupportedError('Octal characters not supported yet');
-    });
-
-  }
+  stringPrimitive() => super.stringPrimitive().map((each) => each[1].join());
+  characterEscape() => super.characterEscape().map((each) => _escapeTable[each[1]]);
+  characterOctal() => super.characterOctal().map((each) {
+    throw new UnsupportedError('Octal characters not supported yet');
+  });
 
 }
