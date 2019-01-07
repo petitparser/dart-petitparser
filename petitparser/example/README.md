c17ca84fd5e6b6e57735aa522dcf271cce5991e2
Lukas Renggli <renggli@gmail.com>
2019-01-07 22:26:23 +0100

    Add a calculator as an example to the main repo

---
+++ petitparser/example/README.md
@@ -0,0 +1,8 @@
+PetitParser Examples
+====================
+
+For more complicated examples see the official [GitHub repository](https://github.com/petitparser/dart-petitparser/tree/master/example).
+ 
+This directory contains the command-line calculator that is described in the introductory tutorial:
+ 
+    dart petitparser/example/calc.dart "1 + 2 * 3"
---
+++ petitparser/example/calc.dart
@@ -0,0 +1,29 @@
+/// Calculator from the tutorial.
+library petitparser.example.calc;
+
+import 'package:petitparser/petitparser.dart';
+
+void main(List<String> arguments) {
+  final number = digit().plus().flatten().trim().map(int.parse);
+  final term = undefined();
+  final prod = undefined();
+  final prim = undefined();
+  final start = term.end();
+  term.set(prod
+      .seq(char('+').trim())
+      .seq(term)
+      .map((values) => values[0] + values[2])
+      .or(prod));
+  prod.set(prim
+      .seq(char('*').trim())
+      .seq(prod)
+      .map((values) => values[0] * values[2])
+      .or(prim));
+  prim.set(char('(')
+      .trim()
+      .seq(term)
+      .seq(char(')'.trim()))
+      .map((values) => values[1])
+      .or(number));
+  print(start.parse(arguments.join(' ')).value);
+}
