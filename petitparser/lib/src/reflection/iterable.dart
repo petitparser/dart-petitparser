657b7f395e8ce2fbfc2cafac24a530d1f17ff70e
Lukas Renggli <renggli@gmail.com>
2019-04-09 14:48:49 +0200

    Simplify the parser iterator to be a generator.

--- petitparser/lib/src/reflection/iterable.dart
+++ petitparser/lib/src/reflection/iterable.dart
@@ -14,41 +14,17 @@ import 'package:petitparser/src/core/parser.dart';
 ///       print(each);
 ///     });
 ///
-Iterable<Parser> allParser(Parser root) => ParserIterable(root);
-
-class ParserIterable extends IterableBase<Parser> {
-  final Parser root;
-
-  ParserIterable(this.root);
-
-  @override
-  Iterator<Parser> get iterator => ParserIterator([root]);
-}
-
-class ParserIterator implements Iterator<Parser> {
-  final List<Parser> todo;
-  final Set<Parser> seen;
-
-  ParserIterator(Iterable<Parser> roots)
-      : todo = List.of(roots),
-        seen = Set.of(roots);
-
-  @override
-  Parser current;
-
-  @override
-  bool moveNext() {
-    if (todo.isEmpty) {
-      current = null;
-      return false;
-    }
-    current = todo.removeLast();
-    for (var parser in current.children) {
+Iterable<Parser> allParser(Parser root) sync* {
+  final todo = [root];
+  final seen = Set.of(todo);
+  while (todo.isNotEmpty) {
+    final current = todo.removeLast();
+    yield current;
+    for (final parser in current.children) {
       if (!seen.contains(parser)) {
         todo.add(parser);
         seen.add(parser);
       }
     }
-    return true;
   }
 }
