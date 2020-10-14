f682bb2966f9d3aee91f7c4357512fed1a8a4a3d
Lukas Renggli <renggli@gmail.com>
2020-10-14 21:37:04 +0200

    Add example code of #84 to tests.

--- petitparser/test/regression_test.dart
+++ petitparser/test/regression_test.dart
@@ -62,4 +62,55 @@ void main() {
     expectFailure(parser, '32', 0, '00-31 expected');
     expectFailure(parser, '3', 1, 'digit expected');
   });
+  group('date format parser', () {
+    final day = 'dd'.toParser().map((token) => digit()
+        .repeat(2)
+        .flatten()
+        .map((value) => MapEntry(#day, int.parse(value))));
+    final month = 'mm'.toParser().map((token) => digit()
+        .repeat(2)
+        .flatten()
+        .map((value) => MapEntry(#month, int.parse(value))));
+    final year = 'yyyy'.toParser().map((token) => digit()
+        .repeat(4)
+        .flatten()
+        .map((value) => MapEntry(#year, int.parse(value))));
+
+    final spacing = whitespace().map((token) =>
+        whitespace().star().map((value) => const MapEntry(#unused, 0)));
+    final verbatim = any().map(
+        (token) => token.toParser().map((value) => const MapEntry(#unused, 0)));
+
+    final entries = [day, month, year, spacing, verbatim].toChoiceParser();
+    final format = entries
+        .star()
+        .end()
+        .map((parsers) => parsers.toSequenceParser().map((entries) {
+              final arguments = Map.fromEntries(entries);
+              return DateTime(
+                arguments[#year] ?? DateTime.now().year,
+                arguments[#month] ?? DateTime.january,
+                arguments[#day] ?? 1,
+              );
+            }));
+
+    test('iso', () {
+      final date = format.parse('yyyy-mm-dd').value;
+      expectSuccess(date, '1980-06-11', DateTime(1980, 6, 11));
+      expectSuccess(date, '1982-08-24', DateTime(1982, 8, 24));
+      expectFailure(date, '1984.10.31', 4, '"-" expected');
+    });
+    test('europe', () {
+      final date = format.parse('dd.mm.yyyy').value;
+      expectSuccess(date, '11.06.1980', DateTime(1980, 6, 11));
+      expectSuccess(date, '24.08.1982', DateTime(1982, 8, 24));
+      expectFailure(date, '1984', 2, '"." expected');
+    });
+    test('us', () {
+      final date = format.parse('mm/dd/yyyy').value;
+      expectSuccess(date, '06/11/1980', DateTime(1980, 6, 11));
+      expectSuccess(date, '08/24/1982', DateTime(1982, 8, 24));
+      expectFailure(date, 'Hello', 0, 'digit expected');
+    });
+  });
 }
