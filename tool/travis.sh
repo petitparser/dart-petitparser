0cafcce4eeb2aa3c20b023a078791c238af1853f
Kevin Moore <kevmoo@google.com>
2020-11-22 05:43:22 +0100

    Upgrade to mono_repo v3
    - MUCH better debug output in the script
    - Add self-validate logic
    - Merge analyze stages
    - Move analyze to --fatal-infos (fatal-warnings is automatic)
    
    (cherry picked from commit ba41b59c2851ef00ff1a176da3cdfd045a924cc0)

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,83 +1,122 @@
 #!/bin/bash
-# Created with package:mono_repo v2.3.0
+# Created with package:mono_repo v3.0.0
 
 # Support built in commands on windows out of the box.
-function pub {
-       if [[ $TRAVIS_OS_NAME == "windows" ]]; then
-        command pub.bat "$@"
-    else
-        command pub "$@"
-    fi
+function pub() {
+  if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+    command pub.bat "$@"
+  else
+    command pub "$@"
+  fi
 }
-function dartfmt {
-       if [[ $TRAVIS_OS_NAME == "windows" ]]; then
-        command dartfmt.bat "$@"
-    else
-        command dartfmt "$@"
-    fi
+function dartfmt() {
+  if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+    command dartfmt.bat "$@"
+  else
+    command dartfmt "$@"
+  fi
 }
-function dartanalyzer {
-       if [[ $TRAVIS_OS_NAME == "windows" ]]; then
-        command dartanalyzer.bat "$@"
-    else
-        command dartanalyzer "$@"
-    fi
+function dartanalyzer() {
+  if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+    command dartanalyzer.bat "$@"
+  else
+    command dartanalyzer "$@"
+  fi
 }
 
 if [[ -z ${PKGS} ]]; then
-  echo -e '\033[31mPKGS environment variable must be set!\033[0m'
-  exit 1
+  echo -e '\033[31mPKGS environment variable must be set! - TERMINATING JOB\033[0m'
+  exit 64
 fi
 
 if [[ "$#" == "0" ]]; then
-  echo -e '\033[31mAt least one task argument must be provided!\033[0m'
-  exit 1
+  echo -e '\033[31mAt least one task argument must be provided! - TERMINATING JOB\033[0m'
+  exit 64
 fi
 
-EXIT_CODE=0
+SUCCESS_COUNT=0
+declare -a FAILURES
 
 for PKG in ${PKGS}; do
   echo -e "\033[1mPKG: ${PKG}\033[22m"
-  pushd "${PKG}" || exit $?
+  EXIT_CODE=0
+  pushd "${PKG}" >/dev/null || EXIT_CODE=$?
 
-  PUB_EXIT_CODE=0
-  pub upgrade --no-precompile || PUB_EXIT_CODE=$?
-
-  if [[ ${PUB_EXIT_CODE} -ne 0 ]]; then
-    EXIT_CODE=1
-    echo -e '\033[31mpub upgrade failed\033[0m'
-    popd
-    continue
+  if [[ ${EXIT_CODE} -ne 0 ]]; then
+    echo -e "\033[31mPKG: '${PKG}' does not exist - TERMINATING JOB\033[0m"
+    exit 64
   fi
 
-  for TASK in "$@"; do
-    echo
-    echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
-    case ${TASK} in
-    command_0)
-      echo 'dartanalyzer --enable-experiment=non-nullable --fatal-warnings --fatal-infos .'
-      dartanalyzer --enable-experiment=non-nullable --fatal-warnings --fatal-infos . || EXIT_CODE=$?
-      ;;
-    command_1)
-      echo 'dartfmt --dry-run --set-exit-if-changed .'
-      dartfmt --dry-run --set-exit-if-changed . || EXIT_CODE=$?
-      ;;
-    command_2)
-      echo 'pub run --enable-experiment=non-nullable test --platform vm'
-      pub run --enable-experiment=non-nullable test --platform vm || EXIT_CODE=$?
-      ;;
-    command_3)
-      echo 'pub run --enable-experiment=non-nullable test --platform chrome'
-      pub run --enable-experiment=non-nullable test --platform chrome || EXIT_CODE=$?
-      ;;
-    *)
-      echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
-      EXIT_CODE=1
-      ;;
-    esac
-  done
+  pub upgrade --no-precompile || EXIT_CODE=$?
 
-  popd
+  if [[ ${EXIT_CODE} -ne 0 ]]; then
+    echo -e "\033[31mPKG: ${PKG}; 'pub upgrade' - FAILED  (${EXIT_CODE})\033[0m"
+    FAILURES+=("${PKG}; 'pub upgrade'")
+  else
+    for TASK in "$@"; do
+      EXIT_CODE=0
+      echo
+      echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
+      case ${TASK} in
+      command_0)
+        echo 'dartanalyzer --fatal-infos .'
+        dartanalyzer --fatal-infos . || EXIT_CODE=$?
+        ;;
+      command_1)
+        echo 'dartfmt --dry-run --set-exit-if-changed .'
+        dartfmt --dry-run --set-exit-if-changed . || EXIT_CODE=$?
+        ;;
+      command_2)
+        echo 'pub run test test/all_test.dart --platform vm'
+        pub run test test/all_test.dart --platform vm || EXIT_CODE=$?
+        ;;
+      command_3)
+        echo 'pub run test test/all_test.dart --platform chrome'
+        pub run test test/all_test.dart --platform chrome || EXIT_CODE=$?
+        ;;
+      command_4)
+        echo 'pub global activate dart_coveralls && dart_coveralls report test/all_test.dart'
+        pub global activate dart_coveralls && dart_coveralls report test/all_test.dart || EXIT_CODE=$?
+        ;;
+      command_5)
+        echo 'pub run test --platform vm'
+        pub run test --platform vm || EXIT_CODE=$?
+        ;;
+      command_6)
+        echo 'pub run test --platform chrome'
+        pub run test --platform chrome || EXIT_CODE=$?
+        ;;
+      *)
+        echo -e "\033[31mUnknown TASK '${TASK}' - TERMINATING JOB\033[0m"
+        exit 64
+        ;;
+      esac
+
+      if [[ ${EXIT_CODE} -ne 0 ]]; then
+        echo -e "\033[31mPKG: ${PKG}; TASK: ${TASK} - FAILED (${EXIT_CODE})\033[0m"
+        FAILURES+=("${PKG}; TASK: ${TASK}")
+      else
+        echo -e "\033[32mPKG: ${PKG}; TASK: ${TASK} - SUCCEEDED\033[0m"
+        SUCCESS_COUNT=$((SUCCESS_COUNT + 1))
+      fi
+
+    done
+  fi
+
+  echo
+  echo -e "\033[32mSUCCESS COUNT: ${SUCCESS_COUNT}\033[0m"
+
+  if [ ${#FAILURES[@]} -ne 0 ]; then
+    echo -e "\033[31mFAILURES: ${#FAILURES[@]}\033[0m"
+    for i in "${FAILURES[@]}"; do
+      echo -e "\033[31m  $i\033[0m"
+    done
+  fi
+
+  popd >/dev/null || exit 70
+  echo
 done
 
-exit ${EXIT_CODE}
+if [ ${#FAILURES[@]} -ne 0 ]; then
+  exit 1
+fi
