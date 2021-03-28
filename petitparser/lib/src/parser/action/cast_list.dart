cba04465b05f55dd36ae34303e4088ff8fec2970
Lukas Renggli <renggli@gmail.com>
2021-03-28 15:41:40 +0200

    Add the grammar definition to the tutorial

--- petitparser/lib/src/parser/action/cast_list.dart
+++ petitparser/lib/src/parser/action/cast_list.dart
@@ -3,21 +3,21 @@ import '../../context/result.dart';
 import '../../core/parser.dart';
 import '../combinator/delegate.dart';
 
-extension CastListParserExtension<T> on Parser<List<T>> {
+extension CastListParserExtension<T> on Parser<T> {
   /// Returns a parser that casts itself to `Parser<List<R>>`. Assumes this
   /// parser to be of type `Parser<List>`.
   Parser<List<R>> castList<R>() => CastListParser<T, R>(this);
 }
 
 /// A parser that casts a `Result<List>` to a `Result<List<R>>`.
-class CastListParser<T, R> extends DelegateParser<List<T>, List<R>> {
-  CastListParser(Parser<List<T>> delegate) : super(delegate);
+class CastListParser<T, R> extends DelegateParser<T, List<R>> {
+  CastListParser(Parser<T> delegate) : super(delegate);
 
   @override
   Result<List<R>> parseOn(Context context) {
     final result = delegate.parseOn(context);
     if (result.isSuccess) {
-      return result.success(result.value.cast<R>());
+      return result.success((result.value as List).cast<R>());
     } else {
       return result.failure(result.message);
     }
