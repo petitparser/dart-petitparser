37ba373973ac27ecb7216d13cdf7df1f3fd3c419
Lukas Renggli <renggli@gmail.com>
2021-04-05 12:16:39 +0200

    #105: Fix more of the wording of the delegate parser documentation.

--- petitparser/lib/src/parser/action/flatten.dart
+++ petitparser/lib/src/parser/action/flatten.dart
@@ -4,8 +4,8 @@ import '../../core/parser.dart';
 import '../combinator/delegate.dart';
 
 extension FlattenParserExtension<T> on Parser<T> {
-  /// Returns a parser that discards the result of the receiver, and returns
-  /// the sub-string of the consumed range in the input being parsed.
+  /// Returns a parser that discards the result of the receiver and answers
+  /// the sub-string its delegate consumes.
   ///
   /// If a [message] is provided, the flatten parser can switch to a fast mode
   /// where error tracking within the receiver is suppressed and in case of a
@@ -17,7 +17,7 @@ extension FlattenParserExtension<T> on Parser<T> {
   Parser<String> flatten([String? message]) => FlattenParser<T>(this, message);
 }
 
-/// A parser that discards the result of the delegate, and answers the
+/// A parser that discards the result of the delegate and answers the
 /// sub-string its delegate consumes.
 class FlattenParser<T> extends DelegateParser<T, String> {
   FlattenParser(Parser<T> delegate, [this.message]) : super(delegate);
