7c8c7112155932fb55ef90a3f00933d3a0192b8c
Mikael Djurfeldt <mikael@djurfeldt.com>
2021-04-07 00:34:26 +0200

    Null safety compliance for Quote class.

--- petitparser_examples/lib/src/lisp/quote.dart
+++ petitparser_examples/lib/src/lisp/quote.dart
@@ -1,10 +1,8 @@
-import 'package:petitparser/petitparser.dart';
-
 /// A quoted datum
 ///
 class Quote {
   /// The quoted datum.
-  final Parser datum;
+  Object? datum;
   
   /// Constructs as a quote.
   Quote(this.datum);
