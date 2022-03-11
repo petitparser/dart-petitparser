8b82b7c9277294edeeaced8538db0d867f5307f7
Lukas Renggli <renggli@gmail.com>
2022-03-11 22:30:57 +0100

    Attempt at getting coverage for `whitespace()` parser.

--- lib/src/parser/character/whitespace.dart
+++ lib/src/parser/character/whitespace.dart
@@ -13,42 +13,41 @@ class WhitespaceCharPredicate implements CharacterPredicate {
   bool test(int value) {
     if (value < 256) {
       switch (value) {
-        case 9:
-        case 10:
-        case 11:
-        case 12:
-        case 13:
-        case 32:
-        case 133:
-        case 160:
+        case 0x09:
+        case 0x0A:
+        case 0x0B:
+        case 0x0C:
+        case 0x0D:
+        case 0x20:
+        case 0x85:
+        case 0xA0:
           return true;
         default:
           return false;
       }
-    } else {
-      switch (value) {
-        case 5760:
-        case 8192:
-        case 8193:
-        case 8194:
-        case 8195:
-        case 8196:
-        case 8197:
-        case 8198:
-        case 8199:
-        case 8200:
-        case 8201:
-        case 8202:
-        case 8232:
-        case 8233:
-        case 8239:
-        case 8287:
-        case 12288:
-        case 65279:
-          return true;
-        default:
-          return false;
-      }
+    }
+    switch (value) {
+      case 0x1680:
+      case 0x2000:
+      case 0x2001:
+      case 0x2002:
+      case 0x2003:
+      case 0x2004:
+      case 0x2005:
+      case 0x2006:
+      case 0x2007:
+      case 0x2008:
+      case 0x2009:
+      case 0x200A:
+      case 0x2028:
+      case 0x2029:
+      case 0x202F:
+      case 0x205F:
+      case 0x3000:
+      case 0xFEFF:
+        return true;
+      default:
+        return false;
     }
   }
 
