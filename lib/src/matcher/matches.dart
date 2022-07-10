1692e43dcd7b8aca814a1c8c8702e2e1635ad87d
Lukas Renggli <renggli@gmail.com>
2022-07-10 15:13:22 +0200

    Improve comments of MatchesParserExtension.

--- lib/src/matcher/matches.dart
+++ lib/src/matcher/matches.dart
@@ -2,34 +2,35 @@ import '../core/parser.dart';
 import 'matches/matches_iterable.dart';
 
 extension MatchesParserExtension<T> on Parser<T> {
-  /// Returns a _lazy iterable_ over all successful parse results of type [T] of
-  /// the provided [input].
+  /// Returns a _lazy iterable_ over all non-overlapping successful parse
+  /// results of type [T] over the provided [input].
   ///
-  /// If [start] is provided, parsing will start at that character.
+  /// If [start] is provided, parsing will start at that index.
   ///
-  /// By default only non-overlapping parse results will be returned, but this
-  /// can be changed by setting [overlapping] to `true`.
+  /// If [overlapping] is set to `true`, the parsing is attempted at each input
+  /// position and does not skip over previous matches.
   ///
   /// For example, `letter().plus().flatten().allMatches('abc de')` results
-  /// in the iterable `['abc', 'de']`.
+  /// in the iterable `['abc', 'de']`. `letter().plus().allMatches('abc de',
+  /// overlapping: true)` results in the iterable `['abc', 'bc', 'c',
+  /// 'de', 'e']`.
   Iterable<T> allMatches(String input,
           {int start = 0, bool overlapping = false}) =>
       MatchesIterable<T>(this, input, start, overlapping);
 
-  /// Returns a lazy iterable of all successful overlapping parses of the
-  /// provided [input].
+  /// Returns a list of all successful overlapping parses of [input].
   ///
-  /// For example, `letter().plus().matches('abc de')` results in the iterable
+  /// For example, `letter().plus().matches('abc de')` results in the list
   /// `[['a', 'b', 'c'], ['b', 'c'], ['c'], ['d', 'e'], ['e']]`.
   @Deprecated('Use `allMatches(input, overlapping: true)` instead.')
-  Iterable<T> matches(String input) => allMatches(input, overlapping: true);
+  List<T> matches(String input) =>
+      allMatches(input, overlapping: true).toList();
 
-  /// Returns a lazy iterable of all successful non-overlapping parses of the
-  /// provided [input].
+  /// Returns a list of all successful non-overlapping parses of [input].
   ///
   /// For example, `letter().plus().matchesSkipping('abc de')` results in the
-  /// iterable `[['a', 'b', 'c'], ['d', 'e']]`.
+  /// list `[['a', 'b', 'c'], ['d', 'e']]`.
   @Deprecated('Use `allMatches(input, overlapping: false)` instead.')
-  Iterable<T> matchesSkipping(String input) =>
-      allMatches(input, overlapping: false);
+  List<T> matchesSkipping(String input) =>
+      allMatches(input, overlapping: false).toList();
 }
