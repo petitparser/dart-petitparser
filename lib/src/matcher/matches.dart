53f0dd24606aef01c63c5985234db2a00b783fcd
Lukas Renggli <renggli@gmail.com>
2022-08-07 16:50:08 +0200

    Cleanup documentation and example of `Parser.allMatches()`.

--- lib/src/matcher/matches.dart
+++ lib/src/matcher/matches.dart
@@ -10,26 +10,29 @@ extension MatchesParserExtension<T> on Parser<T> {
   /// If [overlapping] is set to `true`, the parsing is attempted at each input
   /// position and does not skip over previous matches.
   ///
-  /// For example, `letter().plus().flatten().allMatches('abc de')` results
-  /// in the iterable `['abc', 'de']`. `letter().plus().allMatches('abc de',
-  /// overlapping: true)` results in the iterable `['abc', 'bc', 'c',
-  /// 'de', 'e']`.
+  /// For example, with the parser
+  ///
+  ///     final parser = letter().plus().flatten();
+  ///
+  /// `parser.allMatches('abc de')` results in the iterable `['abc', 'de']`; and
+  /// `parser.allMatches('abc de', overlapping: true)` results in the iterable
+  /// `['abc', 'bc', 'c', 'de', 'e']`.
   Iterable<T> allMatches(String input,
           {int start = 0, bool overlapping = false}) =>
       MatchesIterable<T>(this, input, start, overlapping);
 
   /// Returns a list of all successful overlapping parses of [input].
   ///
-  /// For example, `letter().plus().matches('abc de')` results in the list
-  /// `[['a', 'b', 'c'], ['b', 'c'], ['c'], ['d', 'e'], ['e']]`.
+  /// For example, `letter().plus().flatten().matches('abc de')` results in the
+  /// list `['abc', 'bc', 'c', 'de', 'e']`.
   @Deprecated('Use `allMatches(input, overlapping: true)` instead.')
   List<T> matches(String input) =>
       allMatches(input, overlapping: true).toList();
 
   /// Returns a list of all successful non-overlapping parses of [input].
   ///
-  /// For example, `letter().plus().matchesSkipping('abc de')` results in the
-  /// list `[['a', 'b', 'c'], ['d', 'e']]`.
+  /// For example, `letter().plus().flatten().matchesSkipping('abc de')` results
+  /// in the list `['abc', 'de']`.
   @Deprecated('Use `allMatches(input, overlapping: false)` instead.')
   List<T> matchesSkipping(String input) =>
       allMatches(input, overlapping: false).toList();
