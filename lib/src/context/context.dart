9bf55cb51ea72223c0c1c39e13b39571fc1b2a94
Lukas Renggli <renggli@gmail.com>
2023-03-04 23:52:42 +0100

    Mark `Context` fields for inlining, makes actually a big difference.

--- lib/src/context/context.dart
+++ lib/src/context/context.dart
@@ -1,3 +1,4 @@
+import '../shared/annotations.dart';
 import 'failure.dart';
 import 'result.dart';
 import 'success.dart';
@@ -13,9 +14,13 @@ class Context {
   });
 
   /// The input the parser is being run on.
+  @inlineVm
+  @inlineJs
   final String buffer;
 
   /// The current position in the parser input.
+  @inlineVm
+  @inlineJs
   int position;
 
   /// Whether or not the parse is currently successful.
@@ -25,26 +30,36 @@ class Context {
   ///
   /// If `false`, the parser is currently in a error state and [message] is
   /// expected to hold an explanation.
+  @inlineVm
+  @inlineJs
   bool isSuccess;
 
   /// The currently successful read value.
   ///
   /// The contents of this variable is undefined if [isSuccess] is `false`, or
   /// [isSkip] is `true`.
+  @inlineVm
+  @inlineJs
   dynamic value;
 
   /// The currently read error.
   ///
   /// The contents of this variable is undefined if [isSuccess] is `true`.
+  @inlineVm
+  @inlineJs
   String message;
 
   /// Disables backtracking of errors.
   ///
   /// If `true`, parsers must refrain from backtracking and instead propagate
   /// possible new errors to the caller.
+  @inlineVm
+  @inlineJs
   bool isCut;
 
   /// Converts the current state of the context to a [Result].
+  @inlineVm
+  @inlineJs
   Result<T> toResult<T>() => isSuccess
       ? Success<T>(buffer, position, value)
       : Failure<T>(buffer, position, message);
