7678cbd20e7d73875c8650dbf9731b9c1c99c48b
Lukas Renggli <renggli@gmail.com>
2023-04-23 17:14:53 +0200

    Also test deprecated code.

--- test/reflection_test.dart
+++ test/reflection_test.dart
@@ -1104,6 +1104,12 @@ void main() {
           final result = optimize(parser, rules: rules);
           expect(result, same(parser));
         });
+        test('deprecated code', () {
+          final parser = char('a').settable();
+          // ignore: deprecated_member_use_from_same_package
+          final result = removeSettables(parser);
+          expect(result, same(parser.children[0]));
+        });
       });
       group('remove duplicate', () {
         const rules = [optimize_rules.RemoveDuplicate()];
